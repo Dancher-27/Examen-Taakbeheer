5fb175f1bf9561b5e8d021b415dc089f4ad13f40
Kevin van der Burg <kevin@toscani.nl>
2026-06-25 17:19:14 +0200

    bug fix

--- classes/ActivityLog.php
+++ classes/ActivityLog.php
@@ -48,7 +48,7 @@ class ActivityLog {
         if(!$entry['entiteit_id']) { return ''; }
 
         if(!isset($this->entityMapping[$entry['entiteit']])) {
-            return "#" . $entiteit_id;
+            return "#" . $entry['entiteit_id'];
         }
 
         $args = $this->entityMapping[$entry['entiteit']];
@@ -60,7 +60,7 @@ class ActivityLog {
         $stmt->execute([$entry['entiteit_id']]);
         $entity = $stmt->fetch();
 
-        if(!$entity) { return "#" . $entiteit_id; }
+        if(!$entity) { return "#" .$entry['entiteit_id']; }
         return '"' . $entity['name'] . '"';
     }
 }
